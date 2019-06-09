15b9f439881a8166658fe63dc649d96a556f5da6
patricksferraz <patrick.ferraz@outlook.com>
2019-06-09 03:00:23 -0300

    refactor(postgres): split postgres of app_rest

--- app_rest.py
+++ app_rest.py
@@ -1,21 +1,5 @@
 from flask import Flask, jsonify
-import psycopg2
-import os
-
-POSTGRES_URL = os.environ["POSTGRES_URL"]
-POSTGRES_USER = os.environ["POSTGRES_USER"]
-POSTGRES_PW = os.environ["POSTGRES_PW"]
-POSTGRES_DB = os.environ["POSTGRES_DB"]
-
-
-def connect():
-    return psycopg2.connect(
-        host=POSTGRES_URL,
-        database=POSTGRES_DB,
-        user=POSTGRES_USER,
-        password=POSTGRES_PW,
-    )
-
+from postgres import connect
 
 app = Flask(__name__)
 con = connect()
---
+++ postgres.py
@@ -0,0 +1,16 @@
+import psycopg2
+import os
+
+POSTGRES_URL = os.environ["POSTGRES_URL"]
+POSTGRES_USER = os.environ["POSTGRES_USER"]
+POSTGRES_PW = os.environ["POSTGRES_PW"]
+POSTGRES_DB = os.environ["POSTGRES_DB"]
+
+
+def connect():
+    return psycopg2.connect(
+        host=POSTGRES_URL,
+        database=POSTGRES_DB,
+        user=POSTGRES_USER,
+        password=POSTGRES_PW,
+    )
