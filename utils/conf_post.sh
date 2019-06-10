e11c5ab9ca85f068668895e20877a02b522a7f67
patricksferraz <patrick.ferraz@outlook.com>
2019-06-10 18:45:21 -0300

    refactor(conf_post): changes env in docker commands

--- utils/conf_post.sh
+++ utils/conf_post.sh
@@ -3,10 +3,11 @@ docker network create \
 docker run \
     --network=postgres-network \
     --name postgres-address \
-    -e POSTGRES_PASSWORD=${POSTGRES_PW} \
+    -e POSTGRES_PASSWORD=$POSTGRES_PW \
     -d postgres
 docker run \
+    --network=postgres-network \
     -p 80:80 \
-    -e "PGADMIN_DEFAULT_EMAIL=${PGADMIN_DE}" \
-    -e "PGADMIN_DEFAULT_PASSWORD=${PGADMIN_DP}" \
+    -e PGADMIN_DEFAULT_EMAIL=$PGADMIN_DE \
+    -e PGADMIN_DEFAULT_PASSWORD=$PGADMIN_DP \
     -d dpage/pgadmin4
