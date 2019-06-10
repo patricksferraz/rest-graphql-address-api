62fdb9ee6142ea32215686d621ba70937c4bfff2
patricksferraz <patrick.ferraz@outlook.com>
2019-06-10 15:10:52 -0300

    chore(enviroment): adds enviroment files

---
+++ utils/conf_post.sh
@@ -0,0 +1,12 @@
+docker network create \
+    -d bridge postgres-network
+docker run \
+    --network=postgres-network \
+    --name postgres-address \
+    -e POSTGRES_PASSWORD=${POSTGRES_PW} \
+    -d postgres
+docker run \
+    -p 80:80 \
+    -e "PGADMIN_DEFAULT_EMAIL=${PGADMIN_DE}" \
+    -e "PGADMIN_DEFAULT_PASSWORD=${PGADMIN_DP}" \
+    -d dpage/pgadmin4
