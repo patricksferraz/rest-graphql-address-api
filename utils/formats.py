a240633b04ec83843d4eb0c7b307162dc342ad64
patricksferraz <patrick.ferraz@outlook.com>
2019-06-12 00:43:52 -0300

    feat(formats): adds formats structure and format cities tuples

--- utils/formats.py
+++ utils/formats.py
@@ -1,11 +1,29 @@
+def _format_n0(tuple: list):
+    result = {}
+
+    result["id_estado"] = tuple[0]
+    result["nome_estado"] = tuple[1]
+    result["sigla_estado"] = tuple[2]
+
+    if len(tuple) > 3:
+        result["cidades"] = list(map(_format_n1, tuple[3]))
+
+    return result
+
+
+def _format_n1(tuple: list):
+    result = {}
+
+    result["id_cidade"] = int(tuple[0])
+    result["id_estado"] = int(tuple[1])
+    result["nome_cidade"] = tuple[2]
+
+    return result
+
+
 def state_format(states: list):
-    return list(
-        map(
-            lambda s: {
-                "id_estado": s[0],
-                "nome_estado": s[1],
-                "sigla_estado": s[2],
-            },
-            states,
-        )
-    )
+    return list(map(_format_n0, states))
+
+
+def city_format(cities: list):
+    return list(map(_format_n0, cities))
