8c620182f175db9b19281fe5464d175b4f2375ca
patricksferraz <patrick.ferraz@outlook.com>
2019-06-11 23:43:30 -0300

    refactor(formats): changes state_format structure

--- utils/formats.py
+++ utils/formats.py
@@ -1,4 +1,11 @@
 def state_format(states: list):
     return list(
-        map(lambda s: {"id": s[0], "nome": s[1], "sigla": s[2]}, states)
+        map(
+            lambda s: {
+                "id_estado": s[0],
+                "nome_estado": s[1],
+                "sigla_estado": s[2],
+            },
+            states,
+        )
     )
