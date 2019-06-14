59b22e06d5ecff6fc608ec6606208af97dc38e7b
patricksferraz <patrick.ferraz@outlook.com>
2019-06-14 14:51:29 -0300

    feat(test): adds requests in rest

--- tests/test_rest.py
+++ tests/test_rest.py
@@ -4,8 +4,22 @@ URL = "http://127.0.0.1:5000"
 
 headers = {"content-type": "application/json"}
 
-payload = {"root": {}}
+# STATE
+response = []
+response.append(requests.get(f"{URL}/state", headers=headers))
+response.append(requests.get(f"{URL}/state/Ba", headers=headers))
 
-# root = requests.post(f"{URL}/state", headers=headers, json=payload["root"])
-root = requests.get(f"{URL}/state/Ba", headers=headers, params=payload["root"])
-print(root.json())
+# CITY
+response.append(requests.get(f"{URL}/state/ba/city", headers=headers))
+response.append(requests.get(f"{URL}/state/ba/city/itabuna", headers=headers))
+
+# PLACE
+response.append(
+    requests.get(f"{URL}/state/ba/city/itabuna/place", headers=headers)
+)
+response.append(
+    requests.get(f"{URL}/state/ba/city/itabuna/place/45607336", headers=headers)
+)
+
+for r in response:
+    print(f"{r.json()}\n\n")
